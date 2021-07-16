2d20ecd9e482f025ea0013e773e768cc0d98fd39
“[Kimberly <codinglikeagirl42@gmail.com>
2021-07-15 19:34:40 -0700

    Home page logic, index function coded LCHS 25.5

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, request, session
 from game_logic import *
 import string
+import random
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -8,12 +9,22 @@ app.secret_key = 'K>~EEAnH_x,Z{q.43;NmyQiNz1^Yr7'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    reset_board()
+    if request.method == "POST":
+        num_mines = request.form['num_mines']
+        if num_mines == "" or not num_mines.isdigit():
+            session['num_mines'] = random.randint(5,25)
+        else:
+            session['num_mines'] = int(num_mines)
+        session['mines'] = place_mines(session['num_mines'])
+        return redirect('/play')
+    else:    
+        reset_board()
     return render_template("index.html", page_title = "Play Minesweeper")
 
 @app.route('/play', methods=['GET', 'POST'])
 def play():
-    return render_template("mines.html", page_title = "Mines Remaining: ???")
+    page_title = f"Mines Remaining: {session['num_mines']}"
+    return render_template("mines.html", page_title = page_title)
 
 if __name__ == '__main__':
     app.run()
